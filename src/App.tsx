4a66aa466f7e0773121595ef47d8cbadb90c0d10
aramille1 <aramille@googlemail.com>
2022-10-15 16:56:17 +0200

    feat: added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Button } from "./components/Button";
+import NavBar from "./components/navbar";
 
 function App() {
   return (
+    <div>
+      <NavBar/>
     <div className="flex items-center justify-center h-screen bg-slate-600">
       <h1 className="text-3xl font-bold underline text-center text-white">
         Hello world!
       </h1>
       <Button className="bg-green-500">hello</Button>
+    </div>
     </div>
   );
 }
