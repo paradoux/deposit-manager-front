7689177c813ceced137b6e700e47cebe62db26e1
aramille1 <aramille@googlemail.com>
2022-10-19 15:09:53 +0200

    feat: added profile page

--- src/routes/vaults.tsx
+++ src/routes/vaults.tsx
@@ -5,12 +5,45 @@ const DUMMY_VAULTS = [1, 2, 3];
 const Vaults = () => {
   return (
     <main>
-      <h2>Vaults</h2>
-      <div>
+      {/* Landlord vaults */}
+      <h1 className="flex justify-center text-3xl text-gray-500 py-6">
+        Landlord vaults
+      </h1>
+      <div className="flex justify-center">
         {DUMMY_VAULTS.map((vault) => {
           return (
-            <Link to={vault.toString()} key={vault}>
-              {vault}
+            <Link to={vault.toString()}>
+              <div className="border rounded text-black p-5 m-5 text-center	rounded-lg shadow-lg">
+                <h1 className="text-2xl mb-2">Property address</h1>
+                <p className="m-2">E16PZ</p>
+                <div className="border bg-gray-200">Status</div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="flex justify-center py-24">
+        <Link to="/create">
+          <button className="text-2xl bg-sky-500 text-white	px-4 py-2 rounded-md shadow hover:bg-sky-600">
+            Create Vault
+          </button>
+        </Link>
+      </div>
+
+      {/* Renter vaults */}
+      <h1 className="flex justify-center text-3xl text-gray-500 py-6">
+        Renter vaults
+      </h1>
+      <div className="flex justify-center">
+        {DUMMY_VAULTS.map((vault) => {
+          return (
+            <Link to={vault.toString()}>
+              <div className="border rounded text-black p-5 m-5 text-center	rounded-lg shadow-lg">
+                <h1 className="text-2xl mb-2">Property address</h1>
+                <p className="m-2">E16PZ</p>
+                <div className="border bg-gray-200">Status</div>
+              </div>
             </Link>
           );
         })}
