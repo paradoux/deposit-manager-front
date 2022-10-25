91e3c8694090bbcc0c19170baa6cabe392bd39bd
aramille1 <aramille@googlemail.com>
2022-10-25 21:10:39 +0200

    feat: added vault details page

--- src/components/shadow-box.tsx
+++ src/components/shadow-box.tsx
@@ -6,11 +6,11 @@ const ShadowBox = ({
   className,
   ...rest
 }: React.HTMLProps<HTMLDivElement>) => {
-  /*  Blue RGBA rgba(59, 130, 246, 1) 
+  /*  Blue RGBA rgba(59, 130, 246, 1)
       TEAL RGBA rgba(45,212,191,0.56) */
 
   const shadowBoxClasses = classNames(
-    "bg-black border-[1px] border-blue-500 px-12 py-8 rounded-md shadow-[0px_22px_70px_4px_rgba(59,130,246,0.56)]",
+    "bg-black border-[1px] border-blue-500 px-2 lg:px-12 py-8 rounded-md shadow-[0px_22px_70px_4px_rgba(59,130,246,0.56)]",
     className
   );
 
--- src/routes/vault.tsx
+++ src/routes/vault.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
+import { ShadowBox } from "../components/shadow-box";
 
 const Vault = () => {
-  let { vaultId } = useParams();
+  // let { vaultId } = useParams();
   return (
-    <main>
-      <h2>Vault id: {vaultId}</h2>
-    </main>
+    <div className="mt-12 lg:mt-8 w-full flex flex-col items-center font-wotfard">
+      <ShadowBox className="p-10 mx-2 flex flex-col items-center">
+        <h1 className="text-4xl py-6 font-mono">Your vault</h1>
+        <div className="card max-w-lg m-2 p-6 rounded-lg lg:text-2xl">
+          <p className="mb-2"><span className="text-zinc-400">Owner name:</span> Bob Vance</p>
+          <p className="mb-2"><span className="text-zinc-400">Owner wallet:</span> a2jsh432s9wx</p>
+          <p className="mb-2"><span className="text-zinc-400">Renter wallet:</span> 224sh432s9wx</p>
+          <p className="mb-2"><span className="text-zinc-400">Property address:</span> 0x123…abc</p>
+          <p className="mb-2"><span className="text-zinc-400">End of rental period:</span> 21/06/2023</p>
+          <div className="mt-8 pt-8 flex flex-col justify-between items-center border-t-2 border-slate-200">
+            <p className="text-zinc-400">Vault status:</p>
+            <p className="border p-2 mt-2 text-orange-300 border rounded-lg">
+              Waiting for deposit
+            </p>
+          </div>
+        </div>
+        <button className="px-4 py-2 mb-6 text-center text-white font-bold bg-sky-500 rounded-md shadow hover:bg-sky-600">
+          xxxx
+        </button>
+      </ShadowBox>
+    </div>
   );
 };
 
